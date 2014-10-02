1585003184d10afd9e3f5c3243e04514de1ea56c
hawkeye126 <pete.lower@gmail.com>
2014-10-02 10:39:28 -0700

    adding select statement to exclude execution on home page for certain functions (all currently)

--- custom.js
+++ custom.js
@@ -1,3 +1,99 @@
+
+/*
+#
+#   Sticky Header Function
+#   - Add background color to overide image after certain scroll point
+#
+#   - ONLY WORKS ON HOME PAGE (WHEN BODY HAS CLASS .home)
+*/
+
+  jQuery(window).scroll(function(){
+    if (jQuery('body').hasClass('home')) {
+      //create Variables
+      var amountScrolled = jQuery(window).scrollTop(),
+        headerHeight = jQuery(".site-header").height(),
+        stickyHolderHeight = jQuery(".sticky-holder").height(),
+        changePoint = headerHeight-stickyHolderHeight;//the change point is the point where we add the background color
+
+      //if the user is logged in and can see the admin bar we need to account for that, we add the height of the admin bar to the change point
+      if (jQuery('body').hasClass('admin-bar')) {
+        adminBarHeight = jQuery("#wpadminbar").height();
+        changePoint = changePoint + adminBarHeight;
+      }
+
+      //DEBUG OUTPUT
+      // console.log(
+      //    "Px from top: "+amountScrolled,
+      //    "Header Height: "+headerHeight,
+      //    "Sticky Holder Height: "+stickyHolderHeight,
+      //    "Background Showing: "+(headerHeight-amountScrolled),
+      //    "Change Point: "+changePoint,
+      //    adminBarHeight = jQuery(".admin-bar").height()
+      //    );
+
+      //toggle background image and color
+      if(jQuery(window).scrollTop()<changePoint){
+        jQuery('.sticky-holder-one').css("background","transparent");
+      } else {
+        //jQuery('.sticky-holder-one').toggle("#043789");
+        jQuery('.sticky-holder-one').css("background","#043789");
+        //jQuery('.sticky-holder-one').effect("highlight", {}, 3000)
+      }
+
+
+      //if we hit the breakpoint add top margin and z-index to the autobrands div
+      if(jQuery(window).scrollTop()>changePoint){
+        jQuery('.auto-brands').css("top",amountScrolled-changePoint);
+        jQuery('.auto-brands').css("z-index","200");
+      }
+      else if(jQuery(window).scrollTop()<changePoint) {
+        jQuery('.auto-brands').css("top","0px");
+      }
+    }
+  });
+
+/*
+#
+#   Background image function
+#   http://stackoverflow.com/questions/25342830/remove-white-flash-effect-when-fading-background-images-jquery
+#
+#   - ONLY WORKS ON HOME PAGE (WHEN BODY HAS CLASS .home)
+*/
+
+jQuery(window).load(function () {
+  if (jQuery('body').hasClass('home')) {
+    var i = 0;
+    // var images = [
+    //     'http://dummyimage.com/300x200/FC0/000&text=Slide+1',
+    //     'http://dummyimage.com/300x200/CF0/000&text=Slide+2',
+    //     'http://dummyimage.com/300x200/0FC/000&text=Slide+3'];
+    var  images = ['http://lowermedia.net/europeanmotors/wp-content/blog.dir/sites/44/2014/08/european-motors-auto-service-center-tacoma-seattle-washington-fix-my-car.jpg',
+                'http://europeanmotorsservicecenter.petelower.com/wp-content/uploads/2014/09/IMG_0090.jpg',
+                'http://europeanmotorsservicecenter.petelower.com/wp-content/uploads/2014/09/IMG_0127-3.jpg',
+                'http://europeanmotorsservicecenter.petelower.com/wp-content/uploads/2014/09/fix-my-bmw-tacoma-seattle-washington-audi.jpg'];
+    jQuery('#backgroundimage').css('background-image', 'url(' + images[i] + ')');
+    setInterval(function () {
+        if (++i === images.length) {
+            i = 0;
+        }
+        console.log(i);
+        jQuery('#nextimg').css('background-image', 'url(' + images[i] + ')');
+        // transition animation: 2s
+        jQuery('#backgroundimage').fadeOut(6000, function () {
+            jQuery('#backgroundimage').css('background-image', 'url(' + images[i] + ')').show();
+        });
+        // slide change: 3s
+    }, 9000);
+  }
+});
+
+
+/*
+#
+#    LAND OF DEAD CODE
+#
+*/
+
 // jQuery(window).scroll(function() {
 //     if (jQuery(this).scrollTop() > 200) {
 //         jQuery( ".sticky-back" ).fadeIn();
@@ -16,59 +112,6 @@
 // });
 
 // var divs = jQuery('.sticky-nav');
-
-/*
-#
-#
-#   Sticky Header Function
-#   - Add background color to overide image after certain scroll point
-#
-#
-*/
-
-  jQuery(window).scroll(function(){
-    //create Variables
-    var amountScrolled = jQuery(window).scrollTop(),
-      headerHeight = jQuery(".site-header").height(),
-      stickyHolderHeight = jQuery(".sticky-holder").height(),
-      changePoint = headerHeight-stickyHolderHeight;//the change point is the point where we add the background color
-
-    //if the user is logged in and can see the admin bar we need to account for that, we add the height of the admin bar to the change point
-    if (jQuery('body').hasClass('admin-bar')) {
-      adminBarHeight = jQuery("#wpadminbar").height();
-      changePoint = changePoint + adminBarHeight;
-    }
-
-    //debug output
-    // console.log(
-    //    "Px from top: "+amountScrolled,
-    //    "Header Height: "+headerHeight,
-    //    "Sticky Holder Height: "+stickyHolderHeight,
-    //    "Background Showing: "+(headerHeight-amountScrolled),
-    //    "Change Point: "+changePoint,
-    //    adminBarHeight = jQuery(".admin-bar").height()
-    //    );
-
-    //toggle background image and color
-    if(jQuery(window).scrollTop()<changePoint){
-      jQuery('.sticky-holder-one').css("background","transparent");
-    } else {
-      //jQuery('.sticky-holder-one').toggle("#043789");
-      jQuery('.sticky-holder-one').css("background","#043789");
-      //jQuery('.sticky-holder-one').effect("highlight", {}, 3000)
-    }
-
-    // if (jQuery('body').hasClass('home')) {
-      //if we hit the breakpoint add top margin and z-index to the autobrands div
-      if(jQuery(window).scrollTop()>changePoint){
-        jQuery('.auto-brands').css("top",amountScrolled-changePoint);
-        jQuery('.auto-brands').css("z-index","200");
-      }
-      else if(jQuery(window).scrollTop()<changePoint) {
-        jQuery('.auto-brands').css("top","0px");
-      }
-    // }
-  });
 
 /*
 #
@@ -117,42 +160,3 @@
   //    },1000);
 
   // },5000);
-
-
-
-/*
-#
-#
-#   Background image function
-#   http://stackoverflow.com/questions/25342830/remove-white-flash-effect-when-fading-background-images-jquery
-#
-#
-*/
-
-jQuery(window).load(function () {
-    var i = 0;
-    // var images = [
-    //     'http://dummyimage.com/300x200/FC0/000&text=Slide+1',
-    //     'http://dummyimage.com/300x200/CF0/000&text=Slide+2',
-    //     'http://dummyimage.com/300x200/0FC/000&text=Slide+3'];
-    var  images = ['http://lowermedia.net/europeanmotors/wp-content/blog.dir/sites/44/2014/08/european-motors-auto-service-center-tacoma-seattle-washington-fix-my-car.jpg',
-                'http://europeanmotorsservicecenter.petelower.com/wp-content/uploads/2014/09/IMG_0090.jpg',
-                'http://europeanmotorsservicecenter.petelower.com/wp-content/uploads/2014/09/IMG_0127-3.jpg',
-                'http://europeanmotorsservicecenter.petelower.com/wp-content/uploads/2014/09/fix-my-bmw-tacoma-seattle-washington-audi.jpg'];
-    jQuery('#backgroundimage').css('background-image', 'url(' + images[i] + ')');
-    setInterval(function () {
-        if (++i === images.length) {
-            i = 0;
-        }
-        console.log(i);
-        jQuery('#nextimg').css('background-image', 'url(' + images[i] + ')');
-        // transition animation: 2s
-        jQuery('#backgroundimage').fadeOut(6000, function () {
-            jQuery('#backgroundimage').css('background-image', 'url(' + images[i] + ')').show();
-        });
-        // slide change: 3s
-    }, 9000);
-});
-
-
-
