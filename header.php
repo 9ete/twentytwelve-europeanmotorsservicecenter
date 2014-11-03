9075e21971b0d73ef6a3b765ea9e93ce9d8820f1
hawkeye126 <pete.lower@gmail.com>
2014-11-03 13:00:06 -0800

    hide loading area as unnecessary

--- header.php
+++ header.php
@@ -49,12 +49,6 @@
 				<div class='show-1024'>
 					
 					<div class='mobile-column-left mobile-column mobile-left-menu'>
-						<!-- <ul id='social-menu' class='social-menu menu'>
-							<li class='menu-item facebook-li'><a href='https://www.facebook.com/europeanmotors.wa'>FB</a></li>
-							<li class='menu-item twitter-li'><a href='https://twitter.com/euromotoservcen'>TW</a></li>
-							<li class='menu-item linkedin-li'><a href='https://www.linkedin.com/company/european-motors-service-center'>Pi</a></li>
-							<li class='menu-item google-plus-li'><a href='https://plus.google.com/100002154887378139579/about'>In</a></li>
-						</ul> -->
 						<?php 
 							if(!getMainMenu('mobile-menu-left', true, '')){
 							  $backup = $wp_query;
@@ -79,10 +73,6 @@
 					</div>
 
 					<div class='mobile-column-right mobile-column mobile-left-menu'>
-						<!-- <ul id='customer-menu' class='customer-menu'>
-							<li class='menu-item'><a href='/wp-admin'>Login</a></li>
-							<li class='menu-item'><a href='/schedule-automotive-appointment-lakewood-tacoma-seattle-washington/'>Schedule</a></li>
-						</ul> -->
 						<?php 
 							if(!getMainMenu('mobile-menu-right', true, '')){
 							  $backup = $wp_query;
@@ -125,27 +115,9 @@
 
 			<?php 
 
-				// if(!is_archive()){
-				// 	if (!is_front_page()){
-				// 		randMenu('auto-brands-menu', 7);
-				// 	}
-				// } else {
-				// 	if(!getMainMenu('auto-brands-menu',false)){
-				// 	  $backup = $wp_query;
-				// 	  $wp_query = NULL;
-				// 	  $wp_query = new WP_Query(array('post_type' => 'post'));
-				// 	  getMainMenu('auto-brands-menu',false);
-				// 	  $wp_query = $backup;
-				// 	} 
-				// 	//randMenu('auto-brands-menu', 7);
-				// }
-
-				if(!is_archive()){
-					?>
+				if(!is_archive()){ ?>
 					<section id='auto-brands' class='auto-brands'>
-					<?php 
-						randMenu('auto-brands-menu', 7); 
-					?>
+					<?php randMenu('auto-brands-menu', 7); ?>
 					</section>
 					<?php
 				} else {
@@ -170,34 +142,20 @@
         <div id="slider1_container" style="position: relative; margin: 0 auto;
         top: 0px; left: 0px; width: 1300px; height: 400px; overflow: hidden;">
             <!-- Loading Screen -->
-            <div u="loading" style="position: absolute; top: 0px; left: 0px;">
+            <!-- <div u="loading" style="position: absolute; top: 0px; left: 0px;">
                 <div style="filter: alpha(opacity=70); opacity: 0.7; position: absolute; display: block;
                 top: 0px; left: 0px; width: 100%; height: 100%;">
                 </div>
                 <div style="position: absolute; display: block; background: url(http://dev.petelower.com/system/slider/img/loading.gif) no-repeat center center;
                 top: 0px; left: 0px; width: 100%; height: 100%;">
                 </div>
-            </div>
+            </div> -->
 
             <!-- Slides Container -->
             <div u="slides" style="cursor: move; position: absolute; left: 0px; top: 0px; width: 1300px;
             height: 400px; overflow: hidden;">
                     <div>
                     <img u="image" src="http://europeanmotorsservicecenter.petelower.com//wp-content/themes/twentytwelve-europeanmotorsservicecenter/img/background-front-2500w-1000h.jpg" />
-                    <!-- <div u="caption" t="NO" t3="RTT|2" r3="137.5%" du3="3000" d3="500" style="position: absolute; width: 445px; height: 300px; top: 100px; left: 600px;">
-                    <img src="http://dev.petelower.com/system/slider/img/new-site/c-phone.png" style="position: absolute; width: 445px; height: 300px; top: 0px; left: 0px;" />
-                    <img u="caption" t="CLIP|LR" du="4000" t2="NO" src="http://dev.petelower.com/system/slider/img/new-site/c-jssor-slider.png" style="position: absolute; width: 102px; height: 78px; top: 70px; left: 130px;" />
-                    <img u="caption" t="ZMF|10" t2="NO" src="http://dev.petelower.com/system/slider/img/new-site/c-text.png" style="position: absolute; width: 80px; height: 53px; top: 153px; left: 163px;" />
-                    <img u="caption" t="RTT|10" t2="NO" src="http://dev.petelower.com/system/slider/img/new-site/c-fruit.png" style="position: absolute; width: 140px; height: 90px; top: 60px; left: 220px;" />
-                    <img u="caption" t="T" du="4000" t2="NO" src="http://dev.petelower.com/system/slider/img/new-site/c-navigator.png" style="position: absolute; width: 200px; height: 155px; top: 57px; left: 121px;" />
-                    </div>
-                    <div u="caption" t="RTT|2" r="-75%" du="1600" d="2500" t2="NO" style="position: absolute; width: 470px; height: 220px; top: 120px; left: 650px;">
-                    <img src="http://dev.petelower.com/system/slider/img/new-site/c-phone-horizontal.png" style="position: absolute; width: 470px; height: 220px; top: 0px; left: 0px;" />
-                    <img u="caption" t3="MCLIP|L" du3="2000" src="http://dev.petelower.com/system/slider/img/new-site/c-slide-1.jpg" style="position: absolute; width: 379px; height: 213px; top: 4px; left: 45px;" />
-                    <img u="caption" t="MCLIP|R" du="2000" t2="NO" src="http://dev.petelower.com/system/slider/img/new-site/c-slide-3.jpg" style="position: absolute; width: 379px; height: 213px; top: 4px; left: 45px;" />
-                    <img u="caption" t="RTTL|BR" x="500%" y="500%" du="1000" d="-3000" r="-30%" t3="L" x3="70%" du3="1600" src="http://dev.petelower.com/system/slider/img/new-site/c-finger-pointing.png" style="position: absolute; width: 257px; height: 300px; top: 80px; left: 200px;" />
-                    <img src="http://dev.petelower.com/system/slider/img/new-site/c-navigator-horizontal.png" style="position: absolute; width: 379px; height: 213px; top: 4px; left: 45px;" />
-                    </div> -->
                     <div style="position: absolute; width: 480px; height: 120px; top: 30px; left: 30px; padding: 5px;
                     text-align: left; line-height: 60px; text-transform: uppercase; font-size: 50px;
                     color: #FFFFFF;"><!-- Title Slide #1 -->
@@ -334,7 +292,7 @@
 				<div class='column-left content-column'>
 					<ul id='social-menu' class='social-menu menu'>
 						<li class='menu-item facebook-li'><a target='_blank' class='genericon genericon-facebook' href='https://www.facebook.com/europeanmotors.wa'></a></li>
-						<li class='menu-item instagram-li'><a target='_blank' class='genericon genericon-instagram'  href='https://www.facebook.com/europeanmotors.wa'></a></li>
+						<li class='menu-item instagram-li'><a target='_blank' class='genericon genericon-instagram'  href='https://www.instagram.com/europeanmotorsservicecenter'></a></li>
 						<li class='menu-item google-plus-li'><a target='_blank' class='genericon genericon-googleplus'  href='https://plus.google.com/100002154887378139579/about'></a></li>
 						<li class='menu-item twitter-li'><a target='_blank' class='genericon genericon-twitter'  href='https://twitter.com/euromotoservcen'></a></li>
 						<li class='menu-item youtube-li'><a target='_blank' class='genericon genericon-youtube'  href='https://twitter.com/euromotoservcen'></a></li>
