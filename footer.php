e3b4b0b83f66633f72e216da58a609b10e4798cb
hawkeye126 <pete.lower@gmail.com>
2014-11-04 09:54:22 -0800

    footer cleanup

--- footer.php
+++ footer.php
@@ -12,36 +12,7 @@
 		</div><!-- #main-inner .wrapper-inner -->
 	</div><!-- #main .wrapper -->
 </div><!-- #page -->
-<?php
 
-function return_post_by_tag($post_type, $tag) {
-	wp_reset_query();
-	$query_images_args = array(
-	    'tag' => $tag,
-	    'post_type' => $post_type, 
-	    'post_mime_type' =>'image', 
-	    'post_status' => 'inherit',
-	    'posts_per_page' => -1,
-	    'suppress_filters'=>true,
-	);
-
-	$query_images = new WP_Query( $query_images_args );
-	$images = array();
-	foreach ( $query_images->posts as $image) {
-	    $images[]= wp_get_attachment_url( $image->ID );
-	    ?>
-	    <a href='<?php echo wp_get_attachment_url( $image->ID ); ?>'>
-	    	<img src='<?php echo wp_get_attachment_url( $image->ID ); ?>' />
-	    </a>
-	    <?php
-	}
-
-	//var_dump($images);
-}
-
-//return_post_by_tag('attachment', 'bmw')
-
-?>
 <footer id="colophon" role="contentinfo" class='colophon-footer'>
 	<div class='wrapper'>
 		<div class="site-info footer-column footer-column-one">
@@ -64,14 +35,6 @@ function return_post_by_tag($post_type, $tag) {
 	</div><!--  -->
 </footer><!-- #colophon -->
 
-<?php wp_footer(); 
-
-/*
-
-<?php do_action( 'twentytwelve_credits' ); ?>
-		<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
-
-*/
-?>
+<?php wp_footer(); ?>
 </body>
 </html>
