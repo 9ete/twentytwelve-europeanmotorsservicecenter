31cc3dce9d5555a7687779dd8fd03f788f95c154
hawkeye126 <pete.lower@gmail.com>
2014-11-04 09:52:14 -0800

    hardcode certs menu into footer

--- footer.php
+++ footer.php
@@ -55,6 +55,11 @@ function return_post_by_tag($post_type, $tag) {
 		</div><!--  -->
 		<div class="footer-column footer-column-four">
 			<?php dynamic_sidebar( 'footer_column_four' ); ?>
+			<ul class='automotive-certifications'>
+				<li class='auto-cert-item'><a href='https://www.ase.com/' alt='ASE Certified Automotive Shop Tacoma Lakewood Seattle Washington' description='ASE Certified Motor Technician Tacoma Lakewood Seattle Washington' ><img alt='ASE Certified Automotive Shop Tacoma Lakewood Seattle Washington' description='ASE Certified Motor Technician'  src='wp-content/themes/twentytwelve-europeanmotorsservicecenter/img/automotive-certifications/ase-automotive-service-excellence-certified-european-motors-washington-lakewood-tacoma-fix-my-land-rover.png' /></a></li>
+				<li class='auto-cert-item'><a href='http://www.iatn.net/' alt='iATN Certified Automotive Shop Tacoma Lakewood Seattle Washington' description='iATN Certified Motor Technician Tacoma Lakewood Seattle Washington' ><img alt='iATN Certified Automotive Shop Tacoma Lakewood Seattle Washington' description='iATN Certified Motor Technician' src='wp-content/themes/twentytwelve-europeanmotorsservicecenter/img/automotive-certifications/iatn-international-automotive-technicians-network-certified-european-motors-washington-lakewood-tacoma-fix-my-land-rover.png' /></a></li>
+				<li class='auto-cert-item'><a href='http://asashop.org/' alt='ASA Certified Automotive Shop Tacoma Lakewood Seattle Washington' description='ASA Certified Motor Technician Tacoma Lakewood Seattle Washington' ><img alt='ASA Certified Automotive Shop Tacoma Lakewood Seattle Washington' description='ASA Certified Motor Technician' src='wp-content/themes/twentytwelve-europeanmotorsservicecenter/img/automotive-certifications/asa-automotive-service-association-certified-european-motors-washington-lakewood-tacoma-fix-my-land-rover.png' /></a></li>
+			</ul>
 		</div><!--  -->
 	</div><!--  -->
 </footer><!-- #colophon -->
